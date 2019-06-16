3fe6b2db17a13969c7235b45585df69bc0de0a7c
tobj <bj7908@naver.com>
2019-06-17 01:41:56 +0900

    게시판 만들기(기본적인 틀 잡기(db에있는 내용 php를 통해 불러오기)

---
+++ 게시판 만들기/db.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+    header('Content-Type: text/html; charset=utf-8'); //utf-8인코딩
+    
+    //'DB호스트주소','DB아이디','DB암호','DB이름'
+    $db = new mysqli("localhost","root","1234","board");
+    $db->set_charset("utf-8");
+
+    function mq($sql)
+    {
+        //global: 외부에서 선언된 $sql을 함수내에서 쓸 수 있도록 해줌
+        global $db;
+        return $db->query($sql);
+    }
+?>
